c41d8ea3c3af97597b38ca2e3927844ba2d37cb0
GIOVANNI RAMUNDI <giovanni.ramundi@walgreens.com>
2023-06-26 11:42:11 +0000

    registratzione

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegistrazioneComponent } from './registrazione/registrazione.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent},
+  { path: 'registrazione', component:RegistrazioneComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
